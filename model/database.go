6c5df1dd076776fd7e62cf469b2f75a92b233288
Victor Fong <i.am.victor.fong@gmail.com>
2016-06-02 22:39:05 -0400

    Initialize mysql db connection

--- model/database.go
+++ model/database.go
@@ -1,13 +1,15 @@
 package model
 
 import (
+  "fmt"
   "os"
   "log"
   "errors"
   "encoding/json"
+  "net/url"
 
-  // _ "github.com/go-sql-driver/mysql"
-  // "database/sql"
+  _ "github.com/go-sql-driver/mysql"
+  "database/sql"
 )
 
 type MySql struct {
@@ -23,7 +25,7 @@ type Database interface{
 }
 
 type DatabaseConnection struct{
-
+  db *sql.DB
 }
 
 func (d DatabaseConnection) Init(uri string) error{
@@ -34,9 +36,59 @@ func (d DatabaseConnection) Init(uri string) error{
     return err
   }
 
+  db, err := initConnection(uri)
+  if err != nil {
+    return err
+  }
+  d.db = db
+
   log.Println("Finished Initializing Database")
   return nil
 }
+
+func formattedUrl(url *url.URL) string {
+	return fmt.Sprintf(
+		"%v@tcp(%v)%v?parseTime=true",
+		url.User,
+		url.Host,
+		url.Path,
+	)
+}
+
+func initConnection(uri string) (*sql.DB, error) {
+
+  url, err := url.Parse(uri)
+	if err != nil {
+		log.Fatalln("Error parsing DATABASE_URL", err)
+	}
+
+  databaseUrl := formattedUrl(url)
+
+  db, err := sql.Open("mysql", databaseUrl)
+  if err != nil {
+    return nil, err
+  }
+
+  statement, err := db.Prepare("SELECT 1 FROM testtable LIMIT 1;")
+  if err != nil {
+    return nil, err
+  }
+
+  _, err = statement.Exec()
+  if err != nil {
+    return nil, err
+  }
+  //
+  // log.Printf("res = %v", res)
+  return db, nil
+}
+
+// func isDatabaseInitialized() (bool, error) {
+//   statement, err := db.Prepare("SELECT 1 FROM testtable LIMIT 1;")
+//   if err != nil {
+//     return err
+//   }
+// }
 
 func getURI() (string, error) {
 	envVar := os.Getenv("VCAP_SERVICES")
--- model/database_test.go
+++ model/database_test.go
@@ -44,6 +44,25 @@ var _ = Describe("Unit", func(){
       Expect(err).ToNot(BeNil())
     })
   })
+})
+
+var _ = Describe("Integration", func(){
+  Context("when database connection is valid", func(){
+    PIt("creates a database connection", func(){
+      uri := "mysql2://twitter-auto2:twitter-auto2@localhost:3306/twitter-auto2?reconnect=true"
+      db, err := initConnection(uri)
+      Expect(err).To(BeNil())
+      Expect(db).ToNot(BeNil())
+    })
+  })
+
+  Context("when database connection is invalid", func(){
+    It("returns an error", func(){
+      uri := "mysql://whatever:lalala@localhost:3306/whatever?reconnect=true"
+      _, err := initConnection(uri)
+      Expect(err).ToNot(BeNil())
+    })
+  })
 
 
 })
